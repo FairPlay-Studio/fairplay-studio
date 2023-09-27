f2b3efc26153694d86abe51695ae97932ba26a67
Jay Fehr <jay.fehr@fairplay.studio>
2023-09-27 13:00:36 -0600

    Fixed CSS for live site

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-<div class="font-mono text-black bg-white bg-amber-400">
-    <div class="container p-8 h-screen">
+<div class="font-mono text-black bg-amber-400">
+    <div class="container p-8 h-screen ">
         <div class="p-8 grid h-full place-items-center">
             <h1 class="text-6xl font-bold">FairPlay.Studio</h1>
         </div>        
