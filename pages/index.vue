174cb7eca6549f75680ac863d5579cfacfffdc16
Jay Fehr <jay.fehr@fairplay.studio>
2023-09-27 12:48:39 -0600

    Initial Landing Page

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,10 @@
 <template>
-    <h1>FairPlay.Studio</h1>
+<div class="font-mono text-black bg-white bg-amber-400">
+    <div class="container p-8 h-screen">
+        <div class="p-8 grid h-full place-items-center">
+            <h1 class="text-6xl font-bold">FairPlay.Studio</h1>
+        </div>        
+    </div>
+</div>
 </template>
   
