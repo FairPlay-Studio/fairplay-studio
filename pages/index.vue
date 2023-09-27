bb6e6162949bd35cbba46bf7fbab3fc18005ac71
Jay Fehr <jay.fehr@fairplay.studio>
2023-09-27 13:45:31 -0600

    Fixed scaling issues

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
-<div class="font-mono text-black bg-amber-400">
-    <div class="container p-8 h-screen ">
-        <div class="p-8 grid h-full place-items-center">
-            <h1 class="text-6xl font-bold">FairPlay.Studio</h1>
+    <div class="font-mono text-black bg-amber-400">
+    <div class="container p-8 sm:p-12 md:p-16 lg:p-24 xl:p-32 h-screen">
+        <div class="p-8 sm:p-12 md:p-16 lg:p-24 xl:p-32 grid h-full place-items-center">
+            <h1 class="text-2xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold">FairPlay.Studio</h1>
         </div>        
     </div>
 </div>
+
 </template>
-  
